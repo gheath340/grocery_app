8c45f6e673eae1b3540a5a1ad0b911b8ce709257
Garrett Heath <heathsmail0@gmail.com>
2025-07-30 08:36:36 -0600

    changed supabase file

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,3 @@
-// import { createClient } from '@supabase/supabase-js'
-
-// export const supabase = createClient(
-//   process.env.EXPO_PUBLIC_SUPABASE_URL!,
-//   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
-// )
-
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
